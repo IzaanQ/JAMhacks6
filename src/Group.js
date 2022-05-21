edd8546aef3e61daa0b31e25dfc3f651ab402b6d
izaan Qaiser <izaanqaiser05@gmail.com>
2022-05-21 16:56:57 -0400

    Homepage done

---
+++ src/Group.js
@@ -0,0 +1,26 @@
+import './Group.css';
+
+function Group() {
+  return (
+    <div className = "Group">
+        <div className="nav-bar">
+          <div className="navTitle">
+            <h1>schedula</h1>
+          </div>
+          <div className="navItem">
+            <div className="navThing">
+              <p>settings</p>
+          </div>
+          <div className="navThing">
+              <p>home</p>
+          </div>
+          <div className="navThing">
+            <p>groups</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Group;
