855a520cb7301626eb457693366eb3287adc919f
LaZeAsh <lazeashyt@gmail.com>
2022-05-21 17:22:01 -0400

    FINALLY REACT ROUTER WORKS

---
+++ src/Settings.js
@@ -0,0 +1,9 @@
+import './Settings.css';
+
+function Settings() {
+  return (
+    <p>Hello World!</p>
+  )
+}
+
+export default Settings;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import mongo from './backend';
+import Group from './Group';
+import Settings from './Settings';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import dotenv from 'dotenv';
 // dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path = "" element = {<App />}></Route>
+      <Route path = "/group" element = {<Group />}></Route>
+      <Route path = "/settings" element = {<Settings />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
